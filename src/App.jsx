ad30bb6b53230c10a260bd7ed22d9f2dfbb20f6c
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-12-10 14:46:19 +0530

    Add Header component and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./sections/Header";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="overflow-hidden">
+      <Header />
     </div>
   );
 };
